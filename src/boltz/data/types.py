80e841b6ca97cb687fa65718563e6dbf09952158
Daniel Danciu <daniel@cradle.bio>
2025-07-02 17:52:48 +0200

    mmcif

--- src/boltz/data/types.py
+++ src/boltz/data/types.py
@@ -139,6 +139,7 @@ Chain = [
     ("res_idx", np.dtype("i4")),
     ("res_num", np.dtype("i4")),
     ("cyclic_period", np.dtype("i4")),
+    ("affinity", np.dtype("?")),
 ]
 
 Connection = [
@@ -555,14 +556,6 @@ class TemplateInfo:
 
 
 @dataclass(frozen=True)
-class AffinityInfo:
-    """AffinityInfo datatype."""
-
-    chain_id: int
-    mw: float
-
-
-@dataclass(frozen=True)
 class Record(JSONSerializable):
     """Record datatype."""
 
@@ -573,7 +566,7 @@ class Record(JSONSerializable):
     inference_options: Optional[InferenceOptions] = None
     templates: Optional[list[TemplateInfo]] = None
     md: Optional[MDInfo] = None
-    affinity: Optional[AffinityInfo] = None
+    affinity: Optional[bool] = False
 
 
 ####################################################################################################
